94d2aaadc59b973ef99805eb3655c839aba55b41
Vesperon <sinitchikoduo2@gmail.com>
2025-02-03 08:56:57 +0800

    New Project added: Navigatu Inventory System

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -83,6 +83,35 @@ export function Project() {
             </footer>
           </div>
         </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a href="https://github.com/Vesperon/Navigatu-Intern-Project" target="\_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Navigatu Inventory System</h3>
+              <p>
+              Developed a system that track, manage and monitor the availability, movement and status of items / units MR within the TBI (Navigatu and Tara) and Personnel
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Laravel</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
       </div>
     </Container>
   );
